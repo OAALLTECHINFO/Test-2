992ceff1ac38544650ced12ba157ca60cf573d6f
OAALLTECHINFO <102165261+OAALLTECHINFO@users.noreply.github.com>
2022-06-20 13:45:13 +0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { TextBox } from "fluent-svelte";
+
+	let value = "Default value";
+</script>
+
+<TextBox bind:value />
+
+<style>
+    @import url("https://unpkg.com/fluent-svelte/theme.css");
+
+    /* Some base styles to get things looking right. */
+    :global(body) {
+        background-color: var(--fds-solid-background-base);
+        color: var(--fds-text-primary);
+    }
+</style>
